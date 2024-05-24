cad94530ae6876c4594367f04dfa21f418579c86
Renanjuniior6 <renanjrr679@gmail.com>
2024-05-24 10:54:35 -0300

    upadate commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useRef } from 'react'
-import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { Container, Image, InputLabel, Input} from './styles'
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import Background from '../../assets/background.png'
-import {Link} from 'react-router-dom'
 
 
 export const Container = styled.div`
